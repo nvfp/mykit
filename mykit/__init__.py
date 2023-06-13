b5c42872f3065c6863b7f734ad49d433a33ea593
Nicholas Valentinus <nvfastplease@gmail.com>
2023-06-13 13:02:41 -0700

    trying new strategy to fix run-tests.yml: the problem could be related to 'pkg_resources' rather than the PATH

--- mykit/__init__.py
+++ mykit/__init__.py
@@ -2,8 +2,18 @@ import os as _os
 import pkg_resources as _pr
 
 
-__version__ = _pr.get_distribution('mykit').version
+try:
+    pkg = _pr.get_distribution('mykit')
+except _pr.DistributionNotFound:
+    ## This exception happens during GitHub Actions testing, let's handle it this way.
+    class _Testing:
+        version = 'testing'
+        project_name = 'mykit'
+    pkg = _Testing
+
+
+__version__ = pkg.version
 
 
 LIB_DIR_PTH = _os.path.dirname(_os.path.abspath(__file__))
-LIB_NAME = _pr.get_distribution('mykit').project_name
+LIB_NAME = pkg.project_name
