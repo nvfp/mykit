7354aa35c1d82465aeab2a69758e7012b7ca32fe
Nicholas Valentinus <nvfastplease@gmail.com>
2023-06-13 13:15:39 -0700

    modified mykit/mykit/__main__.py

--- mykit/__main__.py
+++ mykit/__main__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 import argparse
+import os
+import sys
 
-from mykit import __version__, LIB_NAME
+try:
+    from mykit import __version__, LIB_NAME
+except ModuleNotFoundError:
+    ## This is done to handle situations where the 'mykit' isn't installed yet, so it can't be accessed.
+    root_pth = os.path.dirname(os.path.dirname(__file__))
+    sys.path.append(root_pth)
+    from mykit import __version__, LIB_NAME
 
 
 def main():
