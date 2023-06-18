98a8d855442900f5470566a98199f9937cdaba77
Nicholas Valentinus <nvfastplease@gmail.com>
2023-06-17 23:05:39 -0700

    finished updating mykit/__init__.py

--- mykit/__init__.py
+++ mykit/__init__.py
@@ -15,18 +15,13 @@ except _pr.DistributionNotFound:
 __version__ = pkg.version
 
 
-## these will be deprecated soon
-LIB_DIR_PTH = _os.path.dirname(_os.path.abspath(__file__))
-LIB_NAME = pkg.project_name
-
-
 LIB_REPO = 'https://github.com/nvfp/mykit'
 LIB_NAME = 'mykit'
 LIB_DIST_NAME = pkg.project_name  # distribution name
 
-ROOT_DIR_PTH = _os.path.dirname(_os.path.dirname(_os.path.abspath(__file__)))
-DIST_DIR_PTH = _os.path.join(ROOT_DIR_PTH, LIB_DIST_NAME)
-# print(f'DEBUG: ROOT_DIR_PTH = {ROOT_DIR_PTH}')
+_ROOT_DIR_PTH = _os.path.dirname(_os.path.dirname(_os.path.abspath(__file__)))  # should only be used for development
+DIST_DIR_PTH = _os.path.join(_ROOT_DIR_PTH, LIB_DIST_NAME)
+# print(f'DEBUG: _ROOT_DIR_PTH = {_ROOT_DIR_PTH}')
 # print(f'DEBUG: DIST_DIR_PTH = {DIST_DIR_PTH}')
 
 APP_DIR_PTH = _os.path.join(DIST_DIR_PTH, 'app')
