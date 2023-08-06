7411ae1eda914d27e414ee8b7d7f5df41137ea58
Nicholas Valentinus <nvfastplease@gmail.com>
2023-08-06 10:20:46 -0700

    added unit tests for merging_dicts and add_dict_val

--- tests/test_kit/test_utils.py
+++ tests/test_kit/test_utils.py
@@ -4,7 +4,8 @@ from mykit.kit.utils import (
     sort_dict_by_key, sort_dict_by_val,
     get_first_n_dict_items, get_last_n_dict_items,
     reverse_dict,
-    merge_dicts
+    merge_dicts, merging_dicts,
+    add_dict_val
 )
 
 
@@ -323,6 +324,110 @@ class Test__merge_dicts(unittest.TestCase):
             dict2,
             {}
         )
+
+
+class Test__merging_dicts(unittest.TestCase):
+
+    def test_1(self):
+
+        dict1 = {'a': 1, 'b': 2, 'c': 3}
+        dict2 = {'b': 3, 'c': 4, 'd': 5}
+
+        merging_dicts(dict1, dict2)
+
+        self.assertEqual(
+            dict1,
+            {'a': 1, 'b': 5, 'c': 7, 'd': 5}
+        )
+        self.assertEqual(
+            dict2,
+            {'b': 3, 'c': 4, 'd': 5}
+        )
+
+    def test_2(self):
+
+        dict1 = {}
+        dict2 = {'b': 3, 'c': 4, 'd': 5}
+
+        merging_dicts(dict1, dict2)
+
+        self.assertEqual(
+            dict1,
+            {'b': 3, 'c': 4, 'd': 5}
+        )
+        self.assertEqual(
+            dict2,
+            {'b': 3, 'c': 4, 'd': 5}
+        )
+
+    def test_3(self):
+
+        dict1 = {'a': 1, 'b': 2, 'c': 3}
+        dict2 = {}
+
+        merging_dicts(dict1, dict2)
+
+        self.assertEqual(
+            dict1,
+            {'a': 1, 'b': 2, 'c': 3}
+        )
+        self.assertEqual(
+            dict2,
+            {}
+        )
+
+
+class Test__add_dict_val(unittest.TestCase):
+
+    def test_key_exists(self):
+
+        d = {
+            'a': 0,
+            'b': 5,
+        }
+        add_dict_val(d, 'a')
+        expected = {
+            'a': 1,
+            'b': 5,
+        }
+        self.assertEqual(d, expected)
+
+        d = {
+            'a': 0,
+            'b': 5,
+        }
+        add_dict_val(d, 'a', 1000)
+        expected = {
+            'a': 1000,
+            'b': 5,
+        }
+        self.assertEqual(d, expected)
+
+    def test_key_does_not_exist(self):
+
+        d = {
+            'a': 0,
+            'b': 5,
+        }
+        add_dict_val(d, 'c')
+        expected = {
+            'a': 0,
+            'b': 5,
+            'c': 1,
+        }
+        self.assertEqual(d, expected)
+
+        d = {
+            'a': 0,
+            'b': 5,
+        }
+        add_dict_val(d, 'c', 10, 20)
+        expected = {
+            'a': 0,
+            'b': 5,
+            'c': 20,
+        }
+        self.assertEqual(d, expected)
 
 
 if __name__ == '__main__':
