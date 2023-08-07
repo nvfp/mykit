e8806a380ccbea42707971529a4a81e5d61fd2a1
Nicholas Valentinus <nvfastplease@gmail.com>
2023-08-07 11:01:58 -0700

    chore: use underscore when importing

--- mykit/ghactions/eLog.py
+++ mykit/ghactions/eLog.py
@@ -4,11 +4,11 @@ from mykit.kit.color import (
     Hex as _Hex,
     Colored as _Colored
 )
-from mykit.kit.time import TimeFmt
+from mykit.kit.time import TimeFmt as _TimeFmt
 
 
 def _logger(level, color, msg):
-    _sp.run(['echo', f'[{TimeFmt.hour()}] {_Colored(level, color)}: {msg}'])
+    _sp.run(['echo', f'[{_TimeFmt.hour()}] {_Colored(level, color)}: {msg}'])
 
 
 class eL:
