8c8010dbb238c6b1437c43ae39eb0b8f6b560fa0
Nicholas Valentinus <nvfastplease@gmail.com>
2023-08-10 07:10:39 -0700

    bug fixed in test_stream_capture unit test

--- tests/test_kit/test_stream_capture.py
+++ tests/test_kit/test_stream_capture.py
@@ -18,7 +18,7 @@ class Test__StreamCapture(unittest.TestCase):
         with StreamCapture() as captured:
             print(123)
         result = captured.value
-        expected = '132'
+        expected = '123'
         self.assertEqual(result, expected)
 
         with StreamCapture() as captured:
