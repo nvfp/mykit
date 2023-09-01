c44cce7c32f4aeeba96ccc4215c36e21b66d7725
Nicholas Valentinus <nvfastplease@gmail.com>
2023-09-01 12:45:00 -0700

    add utc_offset arg for TimeFmt

--- mykit/kit/time/test.py
+++ mykit/kit/time/test.py
@@ -3,28 +3,38 @@ import unittest
 from mykit.kit.time import TimeFmt
 
 
-## Note: The tests are using UTC timezone (for GitHub Actions VM)
-# from datetime import datetime, timezone
-# desired_date = datetime(2023, 8, 21, 15, 2, 3, tzinfo=timezone.utc)
-# TS = desired_date.timestamp()
-TS = 1692630123.0
-
-
 class Test__TimeFmt(unittest.TestCase):
 
+    def setUp(self):
+        self.timestamp = 1692630123.0
+        self.utc_offset = 0
+
     def test_date(self):
-        result = TimeFmt.date(TS)
+        result = TimeFmt.date(self.timestamp, self.utc_offset)
         expected = 'Aug 21, 2023'
         self.assertEqual(result, expected)
 
     def test_hour(self):
-        result = TimeFmt.hour(TS)
+        result = TimeFmt.hour(self.timestamp, self.utc_offset)
         expected = '15:02:03'
         self.assertEqual(result, expected)
 
     def test_sort(self):
-        result = TimeFmt.sort(TS)
+        result = TimeFmt.sort(self.timestamp, self.utc_offset)
         expected = '20230821_150203'
+        self.assertEqual(result, expected)
+
+    def test_neat(self):
+        result = TimeFmt.neat(self.timestamp, self.utc_offset)
+        expected = '2023 Aug 21, 15:02:03 UTC+0000'
+        self.assertEqual(result, expected)
+
+        result = TimeFmt.neat(self.timestamp, 1.75)
+        expected = '2023 Aug 21, 16:47:03 UTC+0145'
+        self.assertEqual(result, expected)
+
+        result = TimeFmt.neat(self.timestamp, -11.15)
+        expected = '2023 Aug 21, 03:53:03 UTC-1109'
         self.assertEqual(result, expected)
 
 
