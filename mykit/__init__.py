c569352f9f2c31f57a6320e28cde16d93bf80220
Nicholas Valentinus <nvfastplease@gmail.com>
2023-09-05 07:59:37 -0700

    add lock_version

--- mykit/__init__.py
+++ mykit/__init__.py
@@ -1,7 +1,17 @@
 import argparse as _argparse
+from typing import (
+    NoReturn as _NoReturn,
+    Union as _Union,
+)
 
 
 __version__ = '9.1.0'
+
+
+def lock_version(version:str, /) -> _Union[None, _NoReturn]:
+    """Will raise `AssertionError` if the versions don't match"""
+    if version != __version__:
+        raise AssertionError(f"The `mykit` version {__version__} doesn't match the expected {version}.")
 
 
 def _main():
