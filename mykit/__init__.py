bd79653e15a3646e4ab4cceabd19d3040a9c5ad2
Nicholas Valentinus <nvfastplease@gmail.com>
2023-09-05 04:51:59 -0700

    removing useless constants from mykit/__init__.py

--- mykit/__init__.py
+++ mykit/__init__.py
@@ -1,31 +1,15 @@
-import os as _os
-import pkg_resources as _pr
+import argparse as _argparse
 
 
-try:
-    pkg = _pr.get_distribution('mykit')
-except _pr.DistributionNotFound:
-    ## This exception happens during GitHub Actions testing, let's handle it this way.
-    class _Testing:
-        version = 'testing'
-        project_name = 'mykit'
-    pkg = _Testing
+__version__ = '9.1.0'
 
 
-__version__ = pkg.version
+def _main():
 
+    ## Main parsers
+    p = _argparse.ArgumentParser()
 
-LIB_REPO = 'https://github.com/nvfp/mykit'
-LIB_NAME = 'mykit'
-LIB_DIST_NAME = pkg.project_name  # distribution name
+    ## Global optional args
+    p.add_argument('-v', '--version', action='version', version=f'%(prog)s-{__version__}')
 
-_ROOT_DIR_PTH = _os.path.dirname(_os.path.dirname(_os.path.abspath(__file__)))  # should only be used for development
-DIST_DIR_PTH = _os.path.join(_ROOT_DIR_PTH, LIB_DIST_NAME)
-# print(f'DEBUG: _ROOT_DIR_PTH = {_ROOT_DIR_PTH}')
-# print(f'DEBUG: DIST_DIR_PTH = {DIST_DIR_PTH}')
-
-APP_DIR_PTH = _os.path.join(DIST_DIR_PTH, 'app')
-KIT_DIR_PTH = _os.path.join(DIST_DIR_PTH, 'kit')
-REC_DIR_PTH = _os.path.join(DIST_DIR_PTH, 'rec')
-WEB_DIR_PTH = _os.path.join(DIST_DIR_PTH, 'web')
-# print(f'DEBUG: APP_DIR_PTH = {APP_DIR_PTH}')
+    args = p.parse_args()  # Run the parser
