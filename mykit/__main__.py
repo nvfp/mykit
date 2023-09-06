5b346814079790f52fae4dda5cdf3dfe5a1043d1
Nicholas Valentinus <nvfastplease@gmail.com>
2023-09-06 06:34:35 -0700

    tidying entry point modules

--- mykit/__main__.py
+++ mykit/__main__.py
@@ -1,4 +1,17 @@
-from . import _main as main
+import argparse
+
+from . import __version__
+
+
+def main():
+
+    ## Main parsers
+    p = argparse.ArgumentParser()
+
+    ## Global optional args
+    p.add_argument('-v', '--version', action='version', version=f'mykit-{__version__}')
+
+    args = p.parse_args()  # Run the parser
 
 
 if __name__ == '__main__':
