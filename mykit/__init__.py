9c6cdd27e3cf2d0013a50520fcaa8d281fcbc97c
Nicholas <nvfastplease@gmail.com>
2023-09-06 07:19:18 -0700

    tidying up: entry point ; modernizing some module (#13)
    * tidying entry point modules
    
    * modernizing eLog module

--- mykit/__init__.py
+++ mykit/__init__.py
@@ -1,4 +1,3 @@
-import argparse as _argparse
 from typing import (
     NoReturn as _NoReturn,
     Union as _Union,
@@ -12,14 +11,3 @@ def lock_version(version:str, /) -> _Union[None, _NoReturn]:
     """Will raise `AssertionError` if the versions don't match"""
     if version != __version__:
         raise AssertionError(f"The `mykit` version {__version__} doesn't match the expected {version}.")
-
-
-def _main():
-
-    ## Main parsers
-    p = _argparse.ArgumentParser()
-
-    ## Global optional args
-    p.add_argument('-v', '--version', action='version', version=f'mykit-{__version__}')
-
-    args = p.parse_args()  # Run the parser
