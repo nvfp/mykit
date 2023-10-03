e3e4bbc78d480b451e7dfa97c130c125ea33383d
Nicholas Valentinus <nvfastplease@gmail.com>
2023-10-03 05:28:52 -0700

    this is a dev commit: 5 files changed, 160 insertions(+), 24 deletions(-)
     .github/workflows/TurboTest.yml               |   7 +-
    
     mykit/kit/fileops/nice_io/__init__.py         | 130 +++++++++++++++++++++++---
    
     mykit/kit/readable/make_separator/__init__.py |   5 +-
    
     mykit/kit/readable/time_quick/__init__.py     |   5 +-
    
     mykit/kit/readable/time_quick/test.py         |  37 ++++++--
    
    message generated by QWD (github.com/nvfp/qwd)

--- mykit/kit/readable/make_separator/__init__.py
+++ mykit/kit/readable/make_separator/__init__.py
@@ -5,13 +5,14 @@ from typing import (
 
 
 ## birthdate: Oct 2, 2023
-def make_separator(char:str='─', length:_Optional[int]=None) -> str:
+def make_separator(char:str='─', length:_Optional[int]=None, length_backup:int=21) -> str:
     r"""
     Equivalent to `char*_os.get_terminal_size()[0]`
 
     ## Params
     - `char`: the separator character (should be a single char)
     - `length`: the separator length
+    - `length_backup`: Used with `length=None` when an `OSError` arises (check this function code for details)
 
     ## Demo
     >>> sep = make_separator('─')
@@ -25,7 +26,7 @@ def make_separator(char:str='─', length:_Optional[int]=None) -> str:
             ## this exception is raised when calling this function inside a VM.
             ## i dont know why, but i guess it's because there's no "physical" terminal,
             ## so there's no sense of terminal dimensions. is this why this exception occurs?
-            L = 21
+            L = length_backup
     else:
         L = length
 
--- mykit/kit/readable/time_quick/__init__.py
+++ mykit/kit/readable/time_quick/__init__.py
@@ -4,8 +4,11 @@ def time_quick(secs: float, /) -> str:
     """
     Convert seconds into minutes-seconds concise-look style
 
+    ## Params
+    - `secs`: in seconds (>= 0)
+
     ## Examples
     - `quick(61.25)` -> `'1m1.2s'`
     """
-    m, s = divmod(secs, 60)
+    m, s = divmod(abs(secs), 60)
     return f'{int(m)}m{s:.1f}s'
