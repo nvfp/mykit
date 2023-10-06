e1b0f025185fe2be213d501b2a3f566613ab1c6e
Nicholas Valentinus <nvfastplease@gmail.com>
2023-10-05 20:57:51 -0700

    added trim_long_str

--- mykit/kit/misc/get_py_ver/__init__.py
+++ mykit/kit/misc/get_py_ver/__init__.py
@@ -1,6 +1,7 @@
 import sys as _sys
 
 
+## Birthdate: Oct 4, 2023
 def get_py_ver() -> str:
     """Get the current Python version in the expected 'Major.minor.patch' format"""
     info = _sys.version_info
