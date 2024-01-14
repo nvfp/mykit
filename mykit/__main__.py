447036352ded7f95e35c618bcb7a56fcafbde280
Nicholas Valentinus <nvfastplease@gmail.com>
2024-01-14 05:04:36 -0800

    this is a dev commit: 2 files changed, 3 insertions(+), 3 deletions(-)
     _etc/build_configs.json | 2 +-
    
     mykit/__main__.py       | 4 ++--
    
    message generated by QWD (github.com/nvfp/qwd)

--- mykit/__main__.py
+++ mykit/__main__.py
@@ -4,5 +4,5 @@ def main():
     print('hello 123')
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':  # not needed, since the binary `mykit` will execute `main` regardless this line exist or not
+#     main()  # not needed, since the binary `mykit` will execute `main` regardless this line exist or not
