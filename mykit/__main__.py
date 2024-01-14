909bd2b060bfd9f019b172a86e83a224862e13c0
Nicholas Valentinus <nvfastplease@gmail.com>
2024-01-14 10:03:56 -0800

    this is a dev commit: 2 files changed, 158 insertions(+), 17 deletions(-)
     .github/workflows/upload_pypi.yml | 168 ++++++++++++++++++++++++++++++++++----
    
     mykit/__main__.py                 |   7 ++
    
    message generated by QWD (github.com/nvfp/qwd)

--- mykit/__main__.py
+++ mykit/__main__.py
@@ -6,3 +6,10 @@ def main():
 
 # if __name__ == '__main__':  # not needed, since the binary `mykit` will execute `main` regardless this line exist or not
 #     main()  # not needed, since the binary `mykit` will execute `main` regardless this line exist or not
+
+
+## devdocs: the `main` doesnt need to be called here since it will be called via (after downloaded from PyPI):
+## import sys
+## from mykit.__main__ import main
+## sys.exit(main())
+## Read this for more: https://packaging.python.org/en/latest/specifications/entry-points/#use-for-scripts
