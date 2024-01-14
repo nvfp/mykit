83531b325575486bec3494822f91c5f53a40e65c
Nicholas <nvfastplease@gmail.com>
2024-01-14 10:17:31 -0800

    unstable, trying to make the upload_pypi.yml; this is a dev commit: 1 file changed, 58 insertions(+) (#28)
    * this is a dev commit: 1 file changed, 58 insertions(+)
    
     .github/workflows/upload_pypi.yml | 58 +++++++++++++++++++++++++++++++++++++++
    
    message generated by QWD (github.com/nvfp/qwd)
    
    * this is a dev commit: 1 file changed, 7 insertions(+), 3 deletions(-)
    
     .github/workflows/upload_pypi.yml | 10 +++++++---
    
    message generated by QWD (github.com/nvfp/qwd)
    
    * this is a dev commit: 1 file changed, 6 insertions(+), 1 deletion(-)
    
     .github/workflows/upload_pypi.yml | 7 ++++++-
    
    message generated by QWD (github.com/nvfp/qwd)
    
    * this is a dev commit: 1 file changed, 1 insertion(+), 1 deletion(-)
    
     .github/workflows/upload_pypi.yml | 2 +-
    
    message generated by QWD (github.com/nvfp/qwd)
    
    * this is a dev commit: 1 file changed, 2 insertions(+), 1 deletion(-)
    
     .github/workflows/upload_pypi.yml | 3 ++-
    
    message generated by QWD (github.com/nvfp/qwd)
    
    * this is a dev commit: 2 files changed, 19 insertions(+), 4 deletions(-)
    
     .github/workflows/upload_pypi.yml | 13 +++++++++++--
    
     mykit/__main__.py                 | 10 ++++++++--
    
    message generated by QWD (github.com/nvfp/qwd)
    
    * this is a dev commit: 1 file changed, 2 insertions(+), 2 deletions(-)
    
     .github/workflows/upload_pypi.yml | 4 ++--
    
    message generated by QWD (github.com/nvfp/qwd)
    
    * this is a dev commit: 2 files changed, 4 insertions(+), 1 deletion(-)
    
     .github/workflows/upload_pypi.yml | 3 +++
    
     _etc/build_configs.json           | 2 +-
    
    message generated by QWD (github.com/nvfp/qwd)
    
    * this is a dev commit: 2 files changed, 3 insertions(+), 3 deletions(-)
    
     _etc/build_configs.json | 2 +-
    
     mykit/__main__.py       | 4 ++--
    
    message generated by QWD (github.com/nvfp/qwd)
    
    * this is a dev commit: 2 files changed, 2 insertions(+), 2 deletions(-)
    
     _etc/build_configs.json | 2 +-
    
     mykit/__main__.py       | 2 +-
    
    message generated by QWD (github.com/nvfp/qwd)
    
    * this is a dev commit: 2 files changed, 158 insertions(+), 17 deletions(-)
    
     .github/workflows/upload_pypi.yml | 168 ++++++++++++++++++++++++++++++++++----
    
     mykit/__main__.py                 |   7 ++
    
    message generated by QWD (github.com/nvfp/qwd)
    
    * this is a dev commit: 2 files changed, 22 insertions(+), 20 deletions(-)
    
     .github/workflows/run_tests.yml   | 16 ++++++++--------
    
     .github/workflows/upload_pypi.yml | 26 ++++++++++++++------------
    
    message generated by QWD (github.com/nvfp/qwd)

--- mykit/__main__.py
+++ mykit/__main__.py
@@ -1,2 +1,15 @@
-from . import _main as main
-if __name__=='__main__':main()
+
+
+def main():
+    print('hello 123 abc')
+
+
+# if __name__ == '__main__':  # not needed, since the binary `mykit` will execute `main` regardless this line exist or not
+#     main()  # not needed, since the binary `mykit` will execute `main` regardless this line exist or not
+
+
+## devdocs: the `main` doesnt need to be called here since it will be called via (after downloaded from PyPI):
+## import sys
+## from mykit.__main__ import main
+## sys.exit(main())
+## Read this for more: https://packaging.python.org/en/latest/specifications/entry-points/#use-for-scripts
